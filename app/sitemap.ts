781df8c11c1a3c0f6aa6e795003de20c777a9f04
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-10-09 16:05:08 +0600

    minor update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,65 +1,65 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl=process.env.NEXT_PUBLIC_BASE_URL||`https://movie-marathon-2.vercel.app`
+    const baseUrl=process.env.NEXT_PUBLIC_BASE_URL||`https://movie-marathon-2.vercel.app/`
 
     return [
     {
-      url: `${baseUrl}/home`,
+      url: `${baseUrl}home`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: `${baseUrl}/`,
+      url: `${baseUrl}`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/movies`,
+      url: `${baseUrl}movies`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
-        url: `${baseUrl}/tv-shows`,
+        url: `${baseUrl}tv-shows`,
         lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 1,
       },
       {
-        url: `${baseUrl}/genres`,
+        url: `${baseUrl}genres`,
         lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 0.5,
       },
       {
-        url: `${baseUrl}/persons`,
+        url: `${baseUrl}persons`,
         lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 0.5,
       },
       {
-        url: `${baseUrl}/top-contents`,
+        url: `${baseUrl}top-contents`,
         lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 0.5,
       },
       {
-        url: `${baseUrl}/companies`,
+        url: `${baseUrl}companies`,
         lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 0.5,
       },
       {
-        url: `${baseUrl}/country`,
+        url: `${baseUrl}country`,
         lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 0.5,
       },
       {
-        url: `${baseUrl}/search`,
+        url: `${baseUrl}search`,
         lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 0.8,
